f881fcb1153fcf354fbc24ef5b08e18187d6c216
srinidhi <srinidhi1071@gmail.com>
2024-06-01 12:59:02 +0530

    fixed eslint errors

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
                 <div className="flex flex-col xl:flex-row gap-[30px]">
                     <div className="xl:w-[54%] order-2 xl:order-none" >
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-lg">
-                            <h3 className="text-4xl text-accent">Let's work together</h3>
+                            <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
                             <p className="text-white/60">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis modi sint pariatur blanditiis consectetur? Laboriosam quos laborum sint culpa, eius aspernatur eligendi error iure, voluptates vitae atque natus. Illum, similique!</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 <Input type="firstname" placeholder="Firstname" />
--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
-            <h1 className="h1">Hello I'm <br /><span className="text-accent">Srinidhi RV</span></h1>
+            <h1 className="h1">Hello I&apos;m <br /><span className="text-accent">Srinidhi RV</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">I excel at crafiting elegant digital experiences and I am proficient in various programming languages and technologies.</p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
